2934fe844586b67377d8ed66e9480471e341dd0c
kaushani-ea <ea@clapgrow.com>
2026-03-05 17:22:25 +0530

    sauda booking

--- hasco/hasco/doctype/sauda_booking/sauda_booking.js
+++ hasco/hasco/doctype/sauda_booking/sauda_booking.js
@@ -1,8 +1,46 @@
-// Copyright (c) 2026, Pratikshya Gochhayat and contributors
-// For license information, please see license.txt
+frappe.ui.form.on("Sauda Booking Item", {
+    quantity: function(frm, cdt, cdn) {
+        calculate_row(frm, cdt, cdn);
+    },
+    base_price: function(frm, cdt, cdn) {
+        calculate_row(frm, cdt, cdn);
+    },
+    dimension_rate: function(frm, cdt, cdn) {
+        calculate_row(frm, cdt, cdn);
+    },
+    attribute_name: function(frm, cdt, cdn) {   // process rate
+        calculate_row(frm, cdt, cdn);
+    }
+});
 
-// frappe.ui.form.on("Sauda Booking", {
-// 	refresh(frm) {
+function calculate_row(frm, cdt, cdn) {
+    let row = locals[cdt][cdn];
 
-// 	},
-// });
+    let base = flt(row.base_price);
+    let dimension = flt(row.dimension_rate);
+    let process = flt(row.attribute_name);
+    let qty = flt(row.quantity);
+
+    // Rate calculation
+    row.rate = base + dimension + process;
+
+    // Amount calculation
+    row.amount = qty * row.rate;
+
+    frm.refresh_field("table_ulgv");
+
+    calculate_totals(frm);
+}
+
+function calculate_totals(frm) {
+    let total_qty = 0;
+    let total_amt = 0;
+
+    (frm.doc.table_ulgv || []).forEach(function(row) {
+        total_qty += flt(row.quantity);
+        total_amt += flt(row.amount);
+    });
+
+    frm.set_value("total_quantity", total_qty);
+    frm.set_value("total_amount", total_amt);
+}
